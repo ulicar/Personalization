714f52cc5721c3c650852f888e6857bac47494af
Josip Domsic <josip.domsic@gmail.com>
2015-06-23 19:25:03 +0200

    horizontal alligment, pretty

--- collect_files.sh
+++ collect_files.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-cp ~/.bashrc ./bashrc 
-cp ~/.bash_aliases ./bash_aliases
-cp ~/.vimrc ./vimrc
-cp ~/.gitconfig ./gitconfig
-cp ~/.bash_logout ./bash_profile
-cp ~/.bash_profile ./bash_profile
-cp ~/.bash_logout ./bash_logout
+cp ~/.bashrc        ./bashrc 
+cp ~/.bash_aliases  ./bash_aliases
+cp ~/.vimrc         ./vimrc
+cp ~/.gitconfig     ./gitconfig
+cp ~/.bash_logout   ./bash_profile
+cp ~/.bash_profile  ./bash_profile
+cp ~/.bash_logout   ./bash_logout
 
